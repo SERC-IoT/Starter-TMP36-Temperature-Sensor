2395b0eb78b15deda5f5abd8d2aa19cd3063e83f
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-02-17 23:06:38 +0000

    use uppercase for constants. trim trailing whitespace. import time first.

--- esp32/micropython/read_temperature.py
+++ esp32/micropython/read_temperature.py
@@ -1,11 +1,11 @@
 # micropython script for esp32 that reads the voltage from an analogue temperature
 # sensor and convert that reading into degrees centigrade.
-# 
+#
 # The TMP36 uses an analogue voltage with 10mV per degree resolution, with a
 # 500mV offset to allow for negative temeratures.
 
-import machine
 import time
+import machine
 
 # define sensor pin
 SENSOR_PIN = 35
@@ -15,14 +15,14 @@ sensor = machine.ADC(machine.Pin(SENSOR_PIN))
 sensor.atten(machine.ADC.ATTN_0DB)  # 1.2V range
 sensor.width(machine.ADC.WIDTH_12BIT)   # value range 0 to 4095
 
-voltage_range = 1.1
-value_range = 2**12
+VOLTAGE_RANGE = 1.1
+VALUE_RANGE = 2**12
 
 try:
     print("* TMP36 Temperature Readings *")
     while True:
         value = sensor.read()
-        voltage = value * voltage_range / value_range
+        voltage = value * VOLTAGE_RANGE / VALUE_RANGE
         temperature = (voltage - 0.5) * 100
 
         print("Voltage: {0:>5.3f} V. Temperature: {1:>5.3f} C".format(voltage, temperature), end='\r')
--- esp8266/micropython/read_temperature.py
+++ esp8266/micropython/read_temperature.py
@@ -1,25 +1,25 @@
 # micropython script for esp 8266 that reads the voltage from an analogue
 # temperature sensor and convert that reading into degrees centigrade.
-# 
+#
 # The TMP36 uses an analogue voltage with 10mV per degree resolution, with a
 # 500mV offset to allow for negative temeratures.
 
-import machine
 import time
+import machine
 
 # setup analog input
 sensor = machine.ADC(0)
 
 # WeMos D1 mini has a voltage divider on the A0 input. This givens an input
 # voltage range of around 3.0 to 3.2. Measurements have 10-bit depth.
-voltage_range = 3.1 
-value_range = 2**10
+VOLTAGE_RANGE = 3.1
+VALUE_RANGE = 2**10
 
 try:
     print("* TMP36 Temperature Readings *")
     while True:
         value = sensor.read()
-        voltage = value * voltage_range / value_range
+        voltage = value * VOLTAGE_RANGE / VALUE_RANGE
         temperature = (voltage - 0.5) * 100
 
         print("Voltage: {0:>5.3f} V. Temperature: {1:>5.3f} C".format(voltage, temperature), end='\r')
